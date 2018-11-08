5419d2e7f746370dbd58a6136cc5c356230f5f33
Stephen Weiss <stephencweiss@gmail.com>
2018-11-07 17:58:42 -0800

    Routing up to date
    Eliminated any need for routes by serving a single Index.html.
    
    Updated the index.html to serve all four components.
    
    Updated the server to serve a static html when a specific listing is called -- allowing each component to fetch specific information for that listing.

--- server/server.js
+++ server/server.js
@@ -6,6 +6,10 @@ const app = express();
 
 app.use('/', express.static(path.join(__dirname, '../public')));
 
+app.get('/listing/:listingId', (req, res) => {
+  res.sendFile(path.join(__dirname,`../public/index.html`));
+})
+
 app.listen(port, () => {return console.log(`Proxy is listening on port ${port}`) });
 
 
